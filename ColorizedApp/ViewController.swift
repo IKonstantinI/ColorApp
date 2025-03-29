e203594d9eed54b7b9ff5ecac1020eb03bf9e512
Konstantin <diel404@gmail.com>
2025-03-30 00:04:54 +0300

    Create logic app

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,46 +7,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
-    @IBOutlet var setColorView: UIView!
+    @IBOutlet private var colorView: UIView!
     
-    @IBOutlet var redPointLabel: UILabel!
-    @IBOutlet var greenPointLabel: UILabel!
-    @IBOutlet var bluePointLabel: UILabel!
+    @IBOutlet private var redPointLabel: UILabel!
+    @IBOutlet private var greenPointLabel: UILabel!
+    @IBOutlet private var bluePointLabel: UILabel!
     
-    @IBOutlet var redSlider: UISlider!
-    @IBOutlet var greenSlider: UISlider!
-    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet private var redSlider: UISlider!
+    @IBOutlet private var greenSlider: UISlider!
+    @IBOutlet private var blueSlider: UISlider!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setColorView.layer.cornerRadius = 10
+        colorViewSetup()
+        valueLabelSetup()
+        sliderSetup()
+    }
+    
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        switch sender {
+        case redSlider:
+            redPointLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenPointLabel.text = string(from: greenSlider)
+        default:
+            bluePointLabel.text = string(from: blueSlider)
+        }
+        colorViewSetup()
+    }
+    
+    private func valueLabelSetup() {
+        redPointLabel.text = string(from: redSlider)
+        greenPointLabel.text = string(from: greenSlider)
+        bluePointLabel.text = string(from: blueSlider)
+    }
+   
+    private func sliderSetup() {
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         blueSlider.minimumTrackTintColor = .blue
     }
     
-    @IBAction func redSliderAction() {
-        redPointLabel.text = redSlider.value.formatted(.number.precision(.fractionLength(2)))
-        updateColorView()
-    }
-    @IBAction func greenSliderAction() {
-        greenPointLabel.text = greenSlider.value.formatted(.number.precision(.fractionLength(2)))
-        updateColorView()
-    }
-    @IBAction func blueSliderAction() {
-        bluePointLabel.text = blueSlider.value.formatted(.number.precision(.fractionLength(2)))
-        updateColorView()
-    }
-    
-    private func updateColorView() {
-        setColorView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
+    private func colorViewSetup() {
+        colorView.layer.cornerRadius = 10
+        colorView.backgroundColor = UIColor(
+            red: redSlider.value.cgFloat(),
+            green: greenSlider.value.cgFloat(),
+            blue: blueSlider.value.cgFloat(),
             alpha: 1
         )
     }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
 }
 
+extension Float {
+    func cgFloat() -> CGFloat {
+        CGFloat(self)
+    }
+}
